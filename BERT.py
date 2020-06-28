1a0eb80487602b9b4af1038a66ef6b81aaeafd84
Pacerzhujiang0112 <59531363+Pacerzhujiang0112@users.noreply.github.com>
2020-06-28 09:22:15 +0800

    Create BERT.py

---
+++ BERT.py
@@ -0,0 +1,27 @@
+from bert_serving.client import BertClient   #使用bert_as_service调用BERT训练好的词向量
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+corpus = []
+bc = BertClient()
+km = KMeans(n_clusters=2)
+pca = PCA(n_components=2)
+gb = open('C:\\Users\\Administrator\\Desktop\\funNLP-master\\data\\动物词库\\THUOCL_animal.txt',encoding='utf-8').readlines()
+for word in gb[:30]:    #为了方便，每个词库只取了前面30个单词
+    word = word.split('\t')
+    corpus.append(word[0])
+
+fb = open('C:\\Users\\Administrator\\Desktop\\funNLP-master\\data\\地名词库\\THUOCL_diming.txt',encoding='utf-8').readlines()
+for word in fb[:30]:
+    word = word.split('\t')
+    corpus.append(word[0])
+
+vectors = bc.encode(corpus)
+vectors_ = pca.fit_transform(vectors)   #降维到二维
+y_ = km.fit_predict(vectors_)       #聚类
+plt.rcParams['font.sans-serif'] = ['FangSong']    #支持中文，Linux系统下此方法行不通
+plt.scatter(vectors_[:,0],vectors_[:, 1],c=y_)   #将点画在图上
+for i in range(len(corpus)):    #给每个点进行标注
+    plt.annotate(s=corpus[i], xy=(vectors_[:, 0][i], vectors_[:, 1][i]),
+                 xytext=(vectors_[:, 0][i] + 0.1, vectors_[:, 1][i] + 0.1))
+plt.show()
